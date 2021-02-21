bd0ffc82cfe1549a5566cbd80ee7270621282274
=hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-02-20 22:18:03 -0700

    Search method and view bindings

--- app/src/main/java/com/example/android_tip_calculator/GoogleActivity.java
+++ app/src/main/java/com/example/android_tip_calculator/GoogleActivity.java
@@ -1,17 +1,44 @@
 package com.example.android_tip_calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GoogleActivity extends AppCompatActivity {
+
+    // inputs
+    private EditText searchBar;
+
+    // buttons
+    private Button searchButton;
+
+    // webview
+    private WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google);
 
+        // register layout
+        searchBar    = (EditText)findViewById(R.id.search_bar);
+        searchButton = (Button)findViewById(R.id.search_button);
+        web          = (WebView)findViewById(R.id.web_view);
 
+        searchButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get searched text
+                String searchedText = searchBar.getText().toString();
+
+                // launch webview with searched text
+                web.loadUrl(searchedText);
+            }
+        });
     }
 
 }
